889540d6869d75e09e7eca540123d7823c1e88c9
Adrián Barroso Climent <byadrian05@gmail.com>
2024-02-02 08:56:05 +0100

    CREAR
    crear el projecte

---
+++ api_rest_pokerdaw/src/main/java/com/grup/pokerdaw/api_rest_pokerdaw/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.grup.pokerdaw.api_rest_pokerdaw;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiRestPokerdawApplication.class);
+	}
+
+}
