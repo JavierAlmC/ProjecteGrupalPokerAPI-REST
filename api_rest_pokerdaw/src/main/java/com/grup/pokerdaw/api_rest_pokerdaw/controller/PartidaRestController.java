1f751c57ac94c08098da26fa0c8710f2452a3bf3
Faristol <joseptortosa33@gmail.com>
2024-02-14 01:41:51 +0100

    merdes

--- api_rest_pokerdaw/src/main/java/com/grup/pokerdaw/api_rest_pokerdaw/controller/PartidaRestController.java
+++ api_rest_pokerdaw/src/main/java/com/grup/pokerdaw/api_rest_pokerdaw/controller/PartidaRestController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +30,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PartidaRestController {
     private final PartidaService partidaService;
     /*
